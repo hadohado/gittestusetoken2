e5423b103efa65652f391ab8365f146640248f9e
hadohado <hadohado@gmail.com>
2024-01-25 15:45:03 -0800

    Initial Commit

---
+++ gittestusetoken/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gittestusetoken
+//
+//  Created by ha tuong do on 1/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("git test after delete xcode Git acct and re-create new git acct with token, world!")
+        }
+        .padding()
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}
---
+++ gittestusetoken/gittestusetokenApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestusetokenApp.swift
+//  gittestusetoken
+//
+//  Created by ha tuong do on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct gittestusetokenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
